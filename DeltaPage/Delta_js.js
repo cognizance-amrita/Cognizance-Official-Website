03cc775c78a43b3d0a59e6658e8196acbab3cf22
Nehal Khan <128951002+Nehal-Khan-29@users.noreply.github.com>
2025-02-06 21:28:36 +0530

    Add files via upload

--- DeltaPage/Delta_js.js
+++ DeltaPage/Delta_js.js
@@ -1,3 +1,56 @@
+//================================================================ Nav Bar ================================================================
+
+
+function togglecheck() {
+    var screenWidth = window.innerWidth;
+    var menuList = document.getElementById("menulist");
+    var menuListItems = menuList.getElementsByTagName("li");
+  
+    if (screenWidth > 1150) {
+      menuList.style.display = "none";
+      for (var i = 0; i < menuListItems.length; i++) {
+        menuListItems[i].style.display = "none";
+      }
+    }
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    togglecheck(); 
+  });
+  
+  window.addEventListener("resize", function() {
+    togglecheck(); 
+  });
+  
+  
+function toggleMenu() {
+    var menuList = document.getElementById("menulist");
+    var menuListItems = menuList.getElementsByTagName("li");
+    if (menuList.style.display === "none") {
+        menuList.style.display = "block";
+        for (var i = 0; i < menuListItems.length; i++) {
+        menuListItems[i].style.display = "block";
+        }
+    } else {
+        menuList.style.display = "none";
+        for (var i = 0; i < menuListItems.length; i++) {
+        menuListItems[i].style.display = "none";
+        }
+    }
+    }
+
+function Menuoff() {
+    var menuList = document.getElementById("menulist");
+    var menuListItems = menuList.getElementsByTagName("li");
+    if (menuList.style.display === "block") {
+    menuList.style.display = "none";
+    for (var i = 0; i < menuListItems.length; i++) {
+        menuListItems[i].style.display = "none";
+    }
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
 
     //-----------------------------------------------------------------------frontbg--------------------------------------------------------------------
@@ -47,96 +100,46 @@ document.addEventListener('DOMContentLoaded', () => {
     
     //-----------------------------------------------------------------------audio--------------------------------------------------------------------
 
-    document.addEventListener('DOMContentLoaded', () => {
-    window.onload = function() {
-        const audio1 = document.getElementById('myAudio');
-        audio1.play();
-        const icon1 = document.querySelector('.playButton_1 i');
-        icon1.classList.add('rot_anime');
-    };
-    });
-
-    // Function to toggle audio mute/unmute
-    function toggleMute() {
-    var audio = document.getElementById('myAudio');
+document.addEventListener('DOMContentLoaded', () => {
+window.onload = function() {
+    const audio1 = document.getElementById('myAudio');
+    audio1.play();
     const icon1 = document.querySelector('.playButton_1 i');
-    
+    icon1.classList.add('rot_anime');
+};
+});
 
-    if (audio.muted) {
-        audio.muted = false;
-        audio.play(); // Ensure it's playing when unmuted
-        icon1.classList.remove('fa-volume-xmark');
-        icon1.classList.add('fa-volume-high');
-        icon1.classList.add('rot_anime');
-    } else {
-        audio.muted = true;
-        icon1.classList.remove('fa-volume-high');
-        icon1.classList.add('fa-volume-xmark');
-        icon1.classList.remove('rot_anime');
-    }
-    }
+// Function to toggle audio mute/unmute
+function toggleMute() {
+var audio = document.getElementById('myAudio');
+const icon1 = document.querySelector('.playButton_1 i');
+
+
+if (audio.muted) {
+    audio.muted = false;
+    audio.play(); // Ensure it's playing when unmuted
+    icon1.classList.remove('fa-volume-xmark');
+    icon1.classList.add('fa-volume-high');
+    icon1.classList.add('rot_anime');
+} else {
+    audio.muted = true;
+    icon1.classList.remove('fa-volume-high');
+    icon1.classList.add('fa-volume-xmark');
+    icon1.classList.remove('rot_anime');
+}
+}
 
    
       
 
     //-----------------------------------------------------------------------img--------------------------------------------------------------------
 
-    const gallery = document.querySelectorAll('.timecontainer img');
+const gallery = document.querySelectorAll('.timecontainer img');
 
-    gallery.forEach(img => {
-        img.addEventListener('click', () => {
-        window.open(img.src, '_blank');
-        });
+gallery.forEach(img => {
+    img.addEventListener('click', () => {
+    window.open(img.src, '_blank');
     });
+});
       
 
-//================================================================ Nav Bar ================================================================
-
-
-function togglecheck() {
-    var screenWidth = window.innerWidth;
-    var menuList = document.getElementById("menulist");
-    var menuListItems = menuList.getElementsByTagName("li");
-  
-    if (screenWidth > 1150) {
-      menuList.style.display = "none";
-      for (var i = 0; i < menuListItems.length; i++) {
-        menuListItems[i].style.display = "none";
-      }
-    }
-  }
-  document.addEventListener("DOMContentLoaded", function() {
-    togglecheck(); 
-  });
-  
-  window.addEventListener("resize", function() {
-    togglecheck(); 
-  });
-  
-  
-  function toggleMenu() {
-    var menuList = document.getElementById("menulist");
-    var menuListItems = menuList.getElementsByTagName("li");
-    if (menuList.style.display === "none") {
-      menuList.style.display = "block";
-      for (var i = 0; i < menuListItems.length; i++) {
-        menuListItems[i].style.display = "block";
-      }
-    } else {
-      menuList.style.display = "none";
-      for (var i = 0; i < menuListItems.length; i++) {
-        menuListItems[i].style.display = "none";
-      }
-    }
-  }
-  
-  function Menuoff() {
-      var menuList = document.getElementById("menulist");
-      var menuListItems = menuList.getElementsByTagName("li");
-      if (menuList.style.display === "block") {
-        menuList.style.display = "none";
-        for (var i = 0; i < menuListItems.length; i++) {
-          menuListItems[i].style.display = "none";
-        }
-      }
-    }
